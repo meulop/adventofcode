3db0583a879246ef2bd4ed85d3ac733646a98ee2
Dave Taylor <d.v.taylor@leedsmet.ac.uk>
2015-12-03 16:08:08 +0000

    Day 1 solutions

---
+++ src/dec1/brackets.rb
@@ -0,0 +1,12 @@
+@floor=0
+@basement_step=-1
+File.open('input.txt').each_char.with_index do |c, idx|
+  @floor+=1 if c == '('
+  @floor-=1 if c == ')'
+  if (@basement_step == -1 and @floor == -1)
+    @basement_step = idx + 1
+  end
+end
+
+puts "Final floor: #{@floor}"
+puts "Basement step: #{@basement_step}"
