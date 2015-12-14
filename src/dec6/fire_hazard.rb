73ba8a9fe34c86d95309edd00fecf8378704657c
Dave Taylor <d.v.taylor@leedsmet.ac.uk>
2015-12-14 17:58:35 +0000

    Finally a working Dec6 part 1 solution. Could be slicker?

--- src/dec6/fire_hazard.rb
+++ src/dec6/fire_hazard.rb
@@ -8,25 +8,28 @@ require 'pp'
 end
 
 
-pp @light_grid[3,2][1]
-
 File.open('data/dec6/input.txt').each do |line|
   line.match(/([a-z\ ]+)([0-9]+),([0-9]+) through ([0-9]+),([0-9]+)$/) do |m|
     @instr = m[1]
     (@x0,@y0,@x1,@y1) = m[2,4].map {|x| x.to_i }
-    @width = @x1-@x0+1
-    @height = @y1-@y0+1
+    @width = @x1+1-@x0
+    @height = @y1+1-@y0
+    puts "#{@width} #{@height}"
     if @instr == "turn on "
-      @light_grid[@x0,@width][@y0,@height] = @width.times.map{ |x| @height.times.map {|y| true} }
+      @light_grid[@x0..@x1].each {|row| row[@y0..@y1] = @height.times.map { true } }
     elsif @instr == "turn off "
-      @light_grid[@x0,@width][@y0,@height] = @width.times.map{ |x| @height.times.map {|y| false} }
+      @light_grid[@x0..@x1].each {|row| row[@y0..@y1] = @height.times.map { false } }
     elsif @instr == "toggle "
       (@x0..@x1).each { |x| (@y0..@y1).each { |y| @light_grid[x][y] = !@light_grid[x][y] } }
     else
       puts "Invalid instruction #{m[:instr]}"
     end
   end
+  @light_grid.each.with_index do |slice, idx|
+    puts "#{idx} #{slice.length}" if slice.length < 1000
+  end
   puts @light_grid.flatten.select { |x| x }.length
+  puts @light_grid.flatten.length
 end
 
 puts @light_grid.flatten.select { |x| x }.length
