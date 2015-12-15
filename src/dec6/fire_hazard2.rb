6d6a271a56bae8ca2ff69f59bde9de8acc9dc315
Dave Taylor <david.a.taylor@gmail.com>
2015-12-15 21:01:13 +0000

    Working version. Feel like there's a more concise way though?

--- src/dec6/fire_hazard2.rb
+++ src/dec6/fire_hazard2.rb
@@ -1,12 +1,11 @@
 require 'pp'
 
-@light_grid = []
-
-
-@light_grid = 1000.times.map do |x|
-  1000.times.map{ |y| false }
+@light_grid = 1000.times.map do 
+  1000.times.map{ 0 }
 end
 
+puts @light_grid.length
+puts @light_grid.flatten.length
 
 File.open('data/dec6/input.txt').each do |line|
   line.match(/([a-z\ ]+)([0-9]+),([0-9]+) through ([0-9]+),([0-9]+)$/) do |m|
@@ -14,22 +13,17 @@ File.open('data/dec6/input.txt').each do |line|
     (@x0,@y0,@x1,@y1) = m[2,4].map {|x| x.to_i }
     @width = @x1+1-@x0
     @height = @y1+1-@y0
-    puts "#{@width} #{@height}"
     if @instr == "turn on "
-      @light_grid[@x0..@x1].each {|row| row[@y0..@y1] = @height.times.map { true } }
+      (@x0..@x1).each {|x| (@y0..@y1).each { |y| @light_grid[x][y] += 1 } }
     elsif @instr == "turn off "
-      @light_grid[@x0..@x1].each {|row| row[@y0..@y1] = @height.times.map { false } }
+      (@x0..@x1).each {|x| (@y0..@y1).each { |y| @light_grid[x][y] = [0,(@light_grid[x][y] - 1)].max } }
     elsif @instr == "toggle "
-      (@x0..@x1).each { |x| (@y0..@y1).each { |y| @light_grid[x][y] = !@light_grid[x][y] } }
+      (@x0..@x1).each {|x| (@y0..@y1).each { |y| @light_grid[x][y] += 2 } }
     else
       puts "Invalid instruction #{m[:instr]}"
     end
   end
-  @light_grid.each.with_index do |slice, idx|
-    puts "#{idx} #{slice.length}" if slice.length < 1000
-  end
-  puts @light_grid.flatten.select { |x| x }.length
-  puts @light_grid.flatten.length
+
 end
 
-puts @light_grid.flatten.select { |x| x }.length
+pp @light_grid.flatten.inject(:+)
