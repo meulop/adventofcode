24aa54aa4284e3c9be3eae8c95ba58ce1d729055
Dave Taylor <david.a.taylor@gmail.com>
2015-12-20 12:54:06 +0000

    Rewrite after a fair amount of reading the subreddit #cheating

--- src/dec7/assembly_required.rb
+++ src/dec7/assembly_required.rb
@@ -1,36 +1,47 @@
 require 'pp'
 
-wires = Hash.new do |h,k|
-  # Cheat - treat a number as a wire that returns that number as input
+# Cache - effectively pre-populate with integers
+$values = Hash.new do |h,k|
   k.to_i
 end
 
+$parents = Hash.new()
 
 File.open('data/dec7/input.txt').each do |line|
-  line.chomp.match(/((?<i1>[^ ]+) )??((?<op>[^ ]+) )?(?<i2>[^ ]+) -> (?<o1>[^ ]+)/) do |m|
-    wires[m[:o1]] = case m[:op]
-    when "AND"
-      wires[m[:i1]] & wires[m[:i2]]
-    when "OR"
-      wires[m[:i1]] | wires[m[:i2]]
-    when "LSHIFT" then
-      wires[m[:i1]] << wires[m[:i2]]
-    when "RSHIFT" then
-      wires[m[:i1]] >> wires[m[:i2]]
-    when "NOT" then
-      65535 ^ wires[m[:i2]]
-    when nil
-      # No op - just assignment
-      m[:i2].to_i
-    else
-      puts "Unknown operation #{m[:op]}"
-      exit 1
-    end
-    puts "#{m[:o1]} = #{wires[m[:o1]]}"
-  end
-
+  (inwire,outwire) = line.chomp.split(' -> ')
+  $parents[outwire] = inwire.split      
 end
 
 
-pp wires
-puts wires["a"]
+def value(wire)
+  if $values[wire] > 0 or wire == "0"
+    $values[wire]
+  else
+    inputs = $parents[wire]
+    case inputs.length
+    when 1
+      # Assignment x -> y
+      $values[wire] = value(inputs[0])
+    when 2
+      # NOT x -> Y
+      (inputs[0] == "NOT") or die("Unknown op")
+      $values[wire] = 65535 ^ value(inputs[1])
+    when 3
+      (in1,op,in2) = inputs
+      case op
+      when 'AND'
+        $values[wire] = value(in1).to_i & value(in2)
+      when 'OR'
+        $values[wire] = value(in1) | value(in2)
+      when 'LSHIFT'
+        $values[wire] = value(in1) << value(in2)
+      when 'RSHIFT'
+        $values[wire] = value(in1) >> value(in2)
+      else
+        die("Unknown Operation #{op}")
+      end
+    end
+  end
+end
+
+pp value('a')
