c918498457ec374c9f4b03dfe72b031bd0099d98
Dave Taylor <david.a.taylor@gmail.com>
2016-01-12 09:21:11 +0000

    Score calculation added, now just find an algorithm to cover recipe space\!

--- src/dec15/science.rb
+++ src/dec15/science.rb
@@ -8,19 +8,28 @@ class Ingredient
   $props.each do |p|
     attr_accessor p.to_sym
   end
- 
+  attr_accessor :name
   def initialize(name)
     @name=name
   end
 end
 
-
+def ingredient_score(recipe)
+  totscore = $props.map do |p|
+    score = $ings.map do |i|
+      recipe[i.name] * i.send(p)
+    end.reduce(:+)
+    score > 0 ? score : 0
+  end
+  pp totscore
+  totscore.include?(0) ? 0 : totscore.reduce(:*)
+end
 
 OptionParser.new do |opts|
   $props.each do |a|
     opts.on("--[no-]#{a}","Include/exclude #{a} (default to include)") do |p|
       pp p
-      p || props.delete(a)
+      p || $props.delete(a)
     end
   end
 end.parse!
@@ -30,11 +39,10 @@ File.open('data/dec15/input.txt').each do |line|
     i=Ingredient.new(m1[:name])
     m1[:rest].scan(/(?<prop>[a-z]+) (?<val>[-0-9]+)/) do |m2|
       pp m2
-      i.send(m2[0] + '=',m2[1]) if $props.include?(m2[0])
+      i.send(m2[0] + '=',m2[1].to_i) if $props.include?(m2[0])
     end
     $ings << i
   end
 end
 
-pp $ings
-
+pp ingredient_score({"Sprinkles" => 1, "Butterscotch" => 1, "Chocolate" => 90, "Candy" => 8})
